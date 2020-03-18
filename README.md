1d26b63f44a9af6743e305525f3cc3f2cd4c4b4b
Ivan Sotelo <isotelo@controlla.com.mx>
2020-03-18 13:15:48 -0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ivan-sotelo/sat-estado-cfdi
+
+<p align="center">
+<a href="https://travis-ci.org/Krnos/Fire"><img src="https://travis-ci.org/Krnos/Fire.svg?branch=master" alt="Build Status"></a>
+<a href='https://coveralls.io/github/Krnos/Fire?branch=master'><img src='https://coveralls.io/repos/github/Krnos/Fire/badge.svg?branch=master' alt='Coverage Status' /></a>
+<a href="https://packagist.org/packages/Krnos/Fire"><img src="https://poser.pugx.org/Krnos/Fire/d/total.svg" alt="Total Downloads"></a>
+<a href="https://packagist.org/packages/Krnos/Fire"><img src="https://poser.pugx.org/Krnos/Fire/v/stable.svg" alt="Latest Stable Version"></a>
+<a href="https://packagist.org/packages/Krnos/Fire"><img src="https://poser.pugx.org/Krnos/Fire/license.svg" alt="License"></a>
+</p>
---
+++ src/CfdiStateServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace IvanSotelo\SatEstadoCfdi;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Event;
+
+class CfdiStateServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // $this->publishes([
+        //     __DIR__.'/config/fire.php' => config_path('fire.php')
+        // ], 'config');
+
+        // $this->publishes([
+        //     __DIR__.'/migrations' => database_path('migrations')
+        // ], 'migrations');
+
+        // $this->loadTranslationsFrom(__DIR__.'/lang', 'krnos');
+
+        // $this->publishes([
+        //     __DIR__.'/lang' => resource_path('lang/vendor/krnos'),
+        // ], 'translations');
+
+        // Event::subscribe(Listeners\FireEventSubscriber::class);
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // $this->mergeConfigFrom(__DIR__ . '/config/fire.php', 'fire');
+    }
+}
