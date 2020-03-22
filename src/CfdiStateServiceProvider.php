6fbd06daac14ff8a022f324eedbf8dae90f0c756
Ivan Sotelo <isotelo@controlla.com.mx>
2020-03-21 21:20:33 -0700

    fix: cfdi state config

--- src/CfdiStateServiceProvider.php
+++ src/CfdiStateServiceProvider.php
@@ -14,9 +14,9 @@ class CfdiStateServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $this->publishes([
-        //     __DIR__.'/config/fire.php' => config_path('fire.php')
-        // ], 'config');
+        $this->publishes([
+            __DIR__.'/config/cfdi-state.php' => config_path('cfdi-state.php')
+        ], 'config');
 
         // $this->publishes([
         //     __DIR__.'/migrations' => database_path('migrations')
@@ -24,11 +24,6 @@ class CfdiStateServiceProvider extends ServiceProvider
 
         $this->loadTranslations();
 
-        // $this->publishes([
-        //     __DIR__.'/lang' => resource_path('lang/vendor/krnos'),
-        // ], 'translations');
-
-        // Event::subscribe(Listeners\FireEventSubscriber::class);
     }
 
     /**
@@ -38,6 +33,7 @@ class CfdiStateServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->mergeConfigFrom(__DIR__ . '/config/cfdi-state.php', 'cfdi-state');
     }
 
     protected function loadTranslations()
