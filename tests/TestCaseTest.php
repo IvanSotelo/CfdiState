467a1ffbc94c70802637e31da0c0deb50a3cdf79
Ivan Sotelo <isotelo@controlla.com.mx>
2020-03-23 12:30:16 -0700

    test: update tests

--- tests/TestCaseTest.php
+++ tests/TestCaseTest.php
@@ -31,7 +31,24 @@ class TestCaseTest extends OrchestraTestCase
         $expresion = 'https://verificacfdi.facturaelectronica.sat.gob.mx/default.aspx?id=9E1D19A8-A7C4-4879-AEB1-E6F4DD300946&re=CTE180828E84&rr=SAP0108228V3&tt=5800.0&fe=LrCQBA==';
         $cfdi = new CFDIState('test.xml');
         $this->assertSame($expresion, $cfdi->getExpression());
-        dd($cfdi->toJson());
     }
 
+    public function test_can_be_return_status()
+    {
+        $cfdi = new CFDIState('test.xml');
+        $this->assertSame('active', $cfdi->getSatStatus()->cfdi_status);
+    }
+
+    public function test_cant_be_return_status()
+    {
+        $cfdi = new CFDIState('test_fail.xml');
+        $this->assertSame('not_found', $cfdi->getSatStatus()->cfdi_status);
+    }
+
+    public function test_cant_be_get_file()
+    {
+        $cfdi = new CFDIState('test_fail3.xml');
+        dd($cfdi->getSatStatus());
+        $this->assertSame('not_found', $cfdi->getSatStatus()->cfdi_status);
+    }
 }
